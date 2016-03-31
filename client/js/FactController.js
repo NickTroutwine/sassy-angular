9a72953ae2c61f73fb9bfc4fcfb3811e62196029
NickTroutwine <Nat88120@ucmo.edu>
2016-03-30 22:12:04 -0500

    ready to deploy

--- client/js/FactController.js
+++ client/js/FactController.js
@@ -8,12 +8,12 @@ function FactController($http, $scope) {
   	var scopeinj = '';
 		var one = 'AngularJS was originally developed in 2009 by Misko Hevery at Brat Tech LLC.';
 		var two = 'AngularJS is mostly maintained by google.';
-	  var three = 'According to JavaScript analytics service Libscore, AngularJS is used on the websites of Wolfram Alpha, NBC, Walgreens, Intel, Sprint, ABC News, and approximately 8,400 other sites out of 1 million tested in July 2015.';
+	  var three = 'AngularJS is used on the websites of Wolfram Alpha, NBC, Walgreens, Intel, Sprint, ABC News.';
 		var four = 'Angulars original release was October 20, 2010.';
 		var five = 'Sass stands for Syntactically Awesome Stylesheets.';
 		var six = 'initially designed by Hampton Catlin and developed by Natalie Weizenbaum.';
 		var seven = 'Sass first appeared in 2006'
-		var eight = 'The original syntax, called "the indented syntax", uses a syntax similar to Haml.[3] It uses indentation to separate code blocks and newline characters to separate rules. The newer syntax, "SCSS", uses block formatting like that of CSS.'
+		var eight = 'The original syntax, called "the indented syntax", uses a syntax similar to Haml.'
 		var rando = Math.round(Math.random()*7);
 		if(rando == 0){
 			 scopeinj = one
